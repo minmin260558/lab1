56ba4dd561b80c4577f542efd808b9fad52f7410
minmin260558 <minmin260558@gmail.com>
2016-03-08 09:43:48 +0800

    add all

---
+++ A.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+		
+	int n;
+	cin>>n;
+	cout<<n<<" ";	
+
+	for(;n!=1;){
+
+		if(n%2==1)
+			n=n*3+1;
+		else
+			n=n/2;
+
+		cout<<n<<" ";
+	}	
+							
+	cout<<endl;
+
+
+	return 0;
+
+
+}
